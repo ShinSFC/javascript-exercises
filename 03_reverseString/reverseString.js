e1567f01f4500c7bed7df3e6ad218bb770837e4a
ShinpeiH <dctechsf@gmail.com>
2024-07-19 00:32:39 -0700

    finish 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let array = string.split('');
+    let newString = array.reverse();
+    return (newString.join(''));
 };
+
+reverseString('hello, sir!')
 
 // Do not edit below this line
 module.exports = reverseString;
