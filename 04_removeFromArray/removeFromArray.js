2273eab7a6637f3f952783e8f29e41caab3a4f6d
ShinpeiH <dctechsf@gmail.com>
2024-07-22 00:08:20 -0700

    passes 1st test but fails 2nd

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, element) {
+    // console.log(element); // element becomes an array
+    // for (value of element) {
+    // console.log(value);
+    return array.filter( function(value) {
+        return value != element;
+    });
+    
 };
+
+let array = [1, 2, 3, 4];
+let result = removeFromArray(array, 2);
+console.log(result);
 
 // Do not edit below this line
 module.exports = removeFromArray;
