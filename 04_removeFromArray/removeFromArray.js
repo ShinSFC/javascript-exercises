84b6997a90d5c653d04ca524f3e514ccbc642a18
ShinpeiH <dctechsf@gmail.com>
2024-07-23 23:53:45 -0700

    pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,16 @@
 const removeFromArray = function(array, ...element) {
-    // element becomes an array because of ...
+    // element becomes an array because of ...rest argument
     for (let value1 of element) {
         console.log(value1);
-        array = array.filter( function(value2) { // return is needed here or result is undefined
-            return value2 != value1;
+        array = array.filter( function(value2) { 
+            return value2 !== value1;
         });
     }
     return array
 };
   
 
-let result = removeFromArray([1, 2, 3, 4], 2, 3);
+let result = removeFromArray([1, 2, 3, 4], "1", 3);
 console.log(result);
 
 // Do not edit below this line
