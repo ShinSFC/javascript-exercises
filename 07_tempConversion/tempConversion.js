7446eeb5696cc09e898e491ed58194889188fd10
ShinpeiH <dctechsf@gmail.com>
2024-07-26 02:28:01 -0700

    pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let cels = (fahr - 32) * (5 / 9);
+  if (cels % 1 !== 0) { 
+    return Number(cels.toFixed(1));
+  } else {
+    return cels;
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahr = cels * (9 / 5) + 32;
+  if (fahr % 1 !== 0) {
+    return Number(fahr.toFixed(1));
+  } else {
+    return fahr;
+  }
 };
+
 
 // Do not edit below this line
 module.exports = {
