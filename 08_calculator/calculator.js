cf49263062dee38d6dcf12db76c6af6a0d4ecd74
ShinpeiH <dctechsf@gmail.com>
2024-09-15 22:17:56 -0700

    pass up to multiplication

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,17 +1,17 @@
 const add = function(num1, num2) {
-  return Number(num1) + Number(num2);
+  return num1 + num2;
 };
 
 const subtract = function(num1, num2) {
   return num1 - num2;
 };
 
-const sum = function(num1, num2) {
-
+const sum = function(arr) {
+  return arr.reduce((sum, num) => sum + num, 0);
 };
 
-const multiply = function(num1, num2) {
-
+const multiply = function(arr) {
+  return arr.reduce((result, num) => result * num);
 };
 
 const power = function() {
@@ -21,6 +21,9 @@ const power = function() {
 const factorial = function() {
 	
 };
+
+// let result =  sum(1, 2)
+// console.log(result)
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.spec.js
+++ 08_calculator/calculator.spec.js
@@ -2,7 +2,7 @@ const calculator = require('./calculator');
 
 describe('add', () => {
   test('adds 0 and 0', () => {
-    expect(calculator.add(0, 0)).toBe(0);
+    expect(calculator.add(0, 0)).toBe(0); //removed a 2nd 0
   });
 
   test('adds 2 and 2', () => {
@@ -25,25 +25,25 @@ describe('sum', () => {
     expect(calculator.sum([])).toBe(0);
   });
 
-  test.skip('computes the sum of an array of one number', () => {
+  test('computes the sum of an array of one number', () => {
     expect(calculator.sum([7])).toBe(7);
   });
 
-  test.skip('computes the sum of an array of two numbers', () => {
+  test('computes the sum of an array of two numbers', () => {
     expect(calculator.sum([7, 11])).toBe(18);
   });
 
-  test.skip('computes the sum of an array of many numbers', () => {
+  test('computes the sum of an array of many numbers', () => {
     expect(calculator.sum([1, 3, 5, 7, 9])).toBe(25);
   });
 });
 
 describe('multiply', () => {
-  test.skip('multiplies two numbers', () => {
+  test('multiplies two numbers', () => {
     expect(calculator.multiply([2, 4])).toBe(8);
   });
 
-  test.skip('multiplies several numbers', () => {
+  test('multiplies several numbers', () => {
     expect(calculator.multiply([2, 4, 6, 8, 10, 12, 14])).toBe(645120);
   });
 });
