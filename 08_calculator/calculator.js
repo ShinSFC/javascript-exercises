ad949d7a978d80e937d4860c9d8aa585dd409176
ShinpeiH <dctechsf@gmail.com>
2024-09-15 23:23:24 -0700

    finish 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -14,12 +14,21 @@ const multiply = function(arr) {
   return arr.reduce((result, num) => result * num);
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  let result = 1;
+  for (let i = num2; i > 0; i--) {
+    result *= num1;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if (num == 0) return 1;
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // let result =  sum(1, 2)
