abc4bb9ca542b272ec9951227af3e97fa7f8b05e
ShinpeiH <dctechsf@gmail.com>
2024-09-16 00:11:03 -0700

    finish palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.toLowerCase().replace(/[^\w]|_/g, '');
+    if (str == str.split('').reverse().join('')) {
+        return true;
+    } else {
+        return false;
+    }
 };
-
+const alphanumerical = 'abcdefghijklmnopqrstuvwxyz0123456789'
+str.filter((char) => alphanumerical.includes(character))
 // Do not edit below this line
 module.exports = palindromes;
